31fbb71729f64c03444a8ef6734e4bcbbdec406d
shaun <shaunewilliams@gmail.com>
2022-10-18 18:10:49 -0500

    fix remap and replace patch.sh with builtin AnyMap workaround (#1)

--- expo/remap.js
+++ expo/remap.js
@@ -1,28 +1,31 @@
-
 const fs = require('fs')
 const remapping = require('@ampproject/remapping')
 const {AnyMap} = require('@jridgewell/trace-mapping')
 
 const read = f => fs.readFileSync(f,'utf8')
 const write = (f,v) => fs.writeFileSync(f,v)
+const writeJson = (f,v) => write(f,JSON.stringify(v))
 
-// normalize a sourcemap that contains "sections" into one that doesn’t
-// so trace-mapping doesn’t break
-const normalize = s => JSON.stringify(AnyMap(JSON.parse(s)))
+// TARGET source map is:
+// -> merged into METRO source map
+// -> to produce the FIXED source map
 
-const target = 'target/index.js.map'
-const targetNorm = 'target/index.js.map.norm'
-const metro = 'build/metro-ios.map'
-const fixed = 'build/metro-ios-fixed.map'
+// filenames
+const targetFile = 'target/index.js.map'
+const metroFile = 'build/metro-ios.map'
+const fixedFile = 'build/metro-ios-fixed.map'
 
-const remapped = remapping(read(metro),
-  (file,ctx) => {
-    if (file.endsWith('target/index.js')) {
-      const text = normalize(read(target))
-      write(targetNorm, text)
-      return text
-    }
-  }
-)
+// source maps
+const metroMap = read(metroFile)
+const targetMap = AnyMap(read(targetFile)) // required: shadow-cljs "sections" need to be flattened
 
-write(fixed, JSON.stringify(remapped))
+// determines how target file is identified in metro source map
+const isTarget = file => file.endsWith('target/index.js')
+
+// source map loader that returns the target map we wish to merge in
+const loader = (file,ctx) => isTarget(file) ? targetMap : null
+
+// produce a merged source map
+const fixedMap = remapping(metroMap, loader)
+
+writeJson(fixedFile, fixedMap)
